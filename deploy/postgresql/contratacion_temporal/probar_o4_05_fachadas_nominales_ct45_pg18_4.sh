3d4bbb1b96a73b0ab45b157fd9e6f6d20cf7b864
aavidad <avidad@dipgra.es>
2026-07-29 20:40:16 +0200

    test(contratación): propagar fallo de tipo CT45

--- deploy/postgresql/contratacion_temporal/probar_o4_05_fachadas_nominales_ct45_pg18_4.sh
+++ deploy/postgresql/contratacion_temporal/probar_o4_05_fachadas_nominales_ct45_pg18_4.sh
@@ -518,7 +518,7 @@ GRANT TEMPORARY ON DATABASE postgres TO vec_c2d2_registro_runtime;
 SQL
 esperar_fallo 'runtime intenta almacenar un tipo CT40' 42501 \
     'permission denied for type alcance_consulta_rrhh_v1' \
-    psql_admin --command "
+    psql_admin --set ON_ERROR_STOP=1 --command "
         SET SESSION AUTHORIZATION vec_c2d2_registro_runtime;
         CREATE TEMP TABLE no_autorizada_ct45 (
             alcance vec_contratacion_temporal.alcance_consulta_rrhh_v1
